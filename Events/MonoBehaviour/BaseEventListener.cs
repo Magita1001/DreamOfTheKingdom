8c964e24a67c8678d73208f3e26f788483f0ef22
Magita1001 <86965181+Magita1001@users.noreply.github.com>
2025-05-21 21:28:12 +0800

    Update BaseEventListener.cs

--- Events/MonoBehaviour/BaseEventListener.cs
+++ Events/MonoBehaviour/BaseEventListener.cs
@@ -5,7 +5,7 @@ using UnityEngine.Events;
 public class BaseEventListener<T> : MonoBehaviour
 {
     public BaseEventSO<T> eventSO; //获得事件
-    public UnityEvent<T> response; //启动事件
+    public UnityEvent<T> response; //要启动的事件 在Inspeactor窗口中配置
 
     private void OnEnable()
     {
